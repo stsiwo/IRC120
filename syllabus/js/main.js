7ac1297100882fd5e155c810986d4de562be0fc5
sts <33679837+stsiwo@users.noreply.github.com>
2020-05-27 21:13:52 -0700

    Add files via upload

--- syllabus/js/main.js
+++ syllabus/js/main.js
@@ -1,11 +1,22 @@
 /**
  * left nav toggle feature
  **/
-let isLeftNavOpen = true
+let isLeftNavOpen = false
 
+// toggle btn
 let leftNavToggleBtn = document.getElementById("left-nav-toggle-btn")
 
+// close icon
+let leftNavCloseIcon = document.getElementById("left-nav-close-icon")
+
 function toggleLeftNavOpenClickEventHandler(event) {
+  console.log("toggle left nav btn click event handler is called")
+
+  /**
+   * need this one to prevent isLeftNavOpen' value to be change.
+   *   - detectOutsideClickEventHandler use that variable to check the outside click
+   **/
+  event.stopPropagation()
 
   let curViewportWidth = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
 
@@ -16,22 +27,23 @@ function toggleLeftNavOpenClickEventHandler(event) {
 
   let targetLeftNav = document.getElementById("aside")
 
-  if (isLeftNavOpen) {
-    // currently open so close it
+  if (!isLeftNavOpen) {
+    // currently clsoe so open it
     targetLeftNav.style.width = "200px"
     targetLeftNav.style.padding = "0 20px" // need this
 
   } else {
-    // currently close so open it
+    // currently open so close it
     targetLeftNav.style.width = "0"
     targetLeftNav.style.padding = "0" // need this
+    
   }
   isLeftNavOpen = !isLeftNavOpen
 }
 
 leftNavToggleBtn.addEventListener("click", toggleLeftNavOpenClickEventHandler)
+leftNavCloseIcon.addEventListener("click", toggleLeftNavOpenClickEventHandler)
 
-// also close after a link text is clicked
 let linkList = document.getElementsByClassName("aside-link")
 for (let i = 0; i < linkList.length; i ++) {
   linkList[i].addEventListener("click", toggleLeftNavOpenClickEventHandler)
@@ -50,7 +62,25 @@ function scrollSmoothToDes(event) {
   })
 }
 
-
 for (let i = 0; i < linkList.length; i ++) {
   linkList[i].addEventListener("click", scrollSmoothToDes)
 }
+
+/**
+ * outside click detection features for left nav close
+ **/
+function detectOutsideClickEventHandler(event) {
+
+  let targetLeftNav = document.getElementById("aside")
+
+  if (!targetLeftNav.contains(event.target) && isLeftNavOpen) {
+    // outside click detected
+    console.log("outside of left nav is clicked")
+    
+    // programatically call click event of close icon
+    document.getElementById("left-nav-close-icon").dispatchEvent(new Event("click"));
+    return 
+  }
+}
+
+document.addEventListener("click", detectOutsideClickEventHandler)
